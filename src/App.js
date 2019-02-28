48d4294b6790a2121731027d10073bf503bd4cac
Gong <445722156@qq.com>
2019-02-28 18:34:11 +0800

    --

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button  } from 'antd'
+import { Layout, Menu, Icon } from 'antd'
+const { Header, Sider, Content, Footer } = Layout
 
 class App extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Button type="primary">Button</Button>
-        </header>
+      <div>
+        <Layout>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+          >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span>nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span>nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>nav 3</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <Icon
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+              />
+            </Header>
+            <Content style={{
+              margin: '24px 16px', padding: 24, background: '#fff'
+            }}
+            >
+              Content
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Ant Design ©2018 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
       </div>
     );
   }
