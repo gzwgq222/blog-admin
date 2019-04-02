66eed3791f2b551fad2b25a7105ddcc9791cea1a
gq <gq@allpyra.com>
2019-04-02 16:44:39 +0800

    更改代理地址

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(proxy('/api', { 
-       target: 'localhost:3000',
+       target: 'http://127.0.0.1:3000',
        secure: false,
        changeOrigin: true,
        pathRewrite: {
